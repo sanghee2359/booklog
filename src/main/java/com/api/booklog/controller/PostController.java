947fdd7887add7efd51b007739b689cbf4c1ac15
wjdtk <wjdtkdgml7352@naver.com>
2024-06-14 18:39:41 +0900

    feat @NotBlank @Valid 를 사용한 데이터 검증

--- src/main/java/com/api/booklog/controller/PostController.java
+++ src/main/java/com/api/booklog/controller/PostController.java
@@ -1,10 +1,19 @@
 package com.api.booklog.controller;
 
 import com.api.booklog.request.PostCreate;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.springframework.util.ReflectionUtils.getField;
+
 
 @Slf4j
 @RestController
@@ -14,9 +23,19 @@ public class PostController {
         return "HEllo world";
     }
     @PostMapping("/posts")
-    public String post(@RequestBody PostCreate params) {
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) throws Exception {
         log.info("params={}", params);
+        if(result.hasErrors()){
+            List<FieldError> fieldError = result.getFieldErrors();
+            FieldError firstFieldError = fieldError.get(0);
 
-        return "Hello world";
+            String fieldName = firstFieldError.getField();
+            String errorMessage = firstFieldError.getDefaultMessage();
+
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
+        return Map.of();
     }
 }
--- src/main/java/com/api/booklog/request/PostCreate.java
+++ src/main/java/com/api/booklog/request/PostCreate.java
@@ -1,12 +1,17 @@
 package com.api.booklog.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
+@Getter
 @ToString
 public class PostCreate {
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
 }
