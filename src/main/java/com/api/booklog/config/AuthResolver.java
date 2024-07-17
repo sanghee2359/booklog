b12778503682c0834d8a060fccc4ae3c5863b9c4
wjdtk <wjdtkdgml7352@naver.com>
2024-07-17 18:52:33 +0900

    feat. Cookie를 통한 인증 및 검증 1. AuthController - ResponseCookie - 그 외 Cookie 옵션 설정
    2. AuthResolver
    - 쿠키 검증

--- src/main/java/com/api/booklog/config/AuthResolver.java
+++ src/main/java/com/api/booklog/config/AuthResolver.java
@@ -4,13 +4,16 @@ import com.api.booklog.config.data.UserSession;
 import com.api.booklog.domain.Session;
 import com.api.booklog.exception.Unauthorized;
 import com.api.booklog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
     private final SessionRepository sessionRepository;
@@ -21,10 +24,20 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if(accessToken == null || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if(servletRequest == null) {
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if(cookies.length == 0) {
+            log.error("쿠키가 없음");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
         // DB 사용자 확인 작업
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
