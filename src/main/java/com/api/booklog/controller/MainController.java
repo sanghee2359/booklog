211217055fa080b2dc158c720cfc5062d2a2f2f8
wjdtk <wjdtkdgml7352@naver.com>
2024-07-31 18:49:45 +0900

    feat. 스프링 시큐리티 - 5. 로그인 된 사용자 정보 가져오기

--- src/main/java/com/api/booklog/controller/MainController.java
+++ src/main/java/com/api/booklog/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.api.booklog.controller;
 
+import com.api.booklog.config.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class MainController {
     @GetMapping("/")
@@ -11,7 +14,9 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Long id = userPrincipal.getUserId();
+        log.info(">>>>>>> 현재 로그인 된 user ID : {}", id);
         return "사용자 페이지입니다.😊😊";
     }
     @GetMapping("/admin")
