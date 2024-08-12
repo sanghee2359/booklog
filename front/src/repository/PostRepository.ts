b6702bf6fe0b522cf480e31e7c8380e7bd6cc93b
wjdtk <wjdtkdgml7352@naver.com>
2024-08-12 20:04:18 +0900

    feat. frontend - 게시글 1. post 작성 API 연결

---
+++ front/src/repository/PostRepository.ts
@@ -0,0 +1,15 @@
+import HttpRepository from '@/repository/HttpRepository'
+import type Login from '@/entity/user/Login'
+import { inject, singleton } from 'tsyringe'
+import type PostWrite from '@/entity/post/PostWrite'
+@singleton()
+export default class PostRepository {
+  constructor(@inject(HttpRepository) private readonly httpRepository: HttpRepository) {}
+  public write(request: PostWrite) {
+    // class
+    return this.httpRepository.post({
+      path: '/api/posts',
+      body: request
+    })
+  }
+}
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,17 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import PostWrite from '@/entity/post/PostWrite'
+import { container } from 'tsyringe'
+import PostRepository from '@/repository/PostRepository'
+import { ElMessage } from 'element-plus'
+import type HttpError from '@/http/HttpError'
+
+const state = reactive({
+  postWrite: new PostWrite()
+})
+const POST_REPOSITORY = container.resolve(PostRepository)
+function write() {
+  POST_REPOSITORY.write(state.postWrite)
+    .then(() => {
+      ElMessage({ type: 'success', message: '글 등록 완료' })
+      location.href = '/'
+    })
+    .catch((e: HttpError) => {
+      ElMessage({ type: 'error', message: e.getMessage })
+    })
+}
+</script>
 
 <template>
   <el-form label-position="top">
     <el-form-item label="제목">
-      <el-input size="large" placeholder="제목을 입력해주세요" />
+      <el-input v-model="state.postWrite.title" size="large" placeholder="제목을 입력해주세요" />
     </el-form-item>
 
     <el-form-item label="내용">
-      <el-input type="textarea" rows="15" alt="내용" />
+      <el-input v-model="state.postWrite.content" type="textarea" rows="15" alt="내용" />
     </el-form-item>
 
     <el-form-item>
-      <el-button type="primary" style="width: 100%"> 등록완료 </el-button>
+      <el-button type="primary" style="width: 100%" @click="write()"> 등록완료 </el-button>
     </el-form-item>
   </el-form>
 </template>
