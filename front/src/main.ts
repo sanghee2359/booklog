46f27964d5ace0a5674c1da9a0cabc08faea122a
wjdtk <wjdtkdgml7352@naver.com>
2025-02-16 16:06:14 +0900

    feat(frontend) : primevue 설정 추가

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,8 +3,12 @@ import 'reflect-metadata'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import 'flowbite'
 import './assets/tailwind.css' /** Tailwind 스타일 적용 **/
+import '@/assets/styles.scss'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
 import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -15,6 +19,18 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.app-dark'
+    }
+  }
+})
+
+app.use(ToastService)
+app.use(ConfirmationService)
 
 app.use(ElementPlus)
 app.mount('#app')
+app.component('Toast', Toast)
